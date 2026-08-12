4079fb9737ada3ccbdab86c5396c171adcd972ca
Lucas Ferreira <lucas_ferreira2d@hotmail.com>
2026-08-12 08:09:41 -0300

    refactor: move estrutura de features para src/app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'processos', pathMatch: 'full' },
+  {
+    path: 'processos',
+    loadComponent: () =>
+      import('./features/processos/processo-list/processo-list')
+        .then(m => m.ProcessoList)
+  },
+  { path: '**', redirectTo: 'processos' }
+];
